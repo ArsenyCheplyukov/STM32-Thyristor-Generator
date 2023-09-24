0830eccc49aa760877775021c15960c41a0c1cde
Sorowful Dreamer <cheplucov32@gmail.com>
2023-09-24 11:40:47 +0300

    Initial commit. Moved from other device

---
+++ Core/Inc/main.h
@@ -0,0 +1,86 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define MaximumVoltage_Pin GPIO_PIN_0
+#define MaximumVoltage_GPIO_Port GPIOA
+#define CurrentVoltage_Pin GPIO_PIN_1
+#define CurrentVoltage_GPIO_Port GPIOA
+#define MaximumAmperage_Pin GPIO_PIN_2
+#define MaximumAmperage_GPIO_Port GPIOA
+#define CurrentAmperage_Pin GPIO_PIN_3
+#define CurrentAmperage_GPIO_Port GPIOA
+#define PWM_Generation_Pin GPIO_PIN_8
+#define PWM_Generation_GPIO_Port GPIOA
+#define PWM_ChangeCapture_Pin GPIO_PIN_9
+#define PWM_ChangeCapture_GPIO_Port GPIOA
+#define PWM_StartCapture_Pin GPIO_PIN_10
+#define PWM_StartCapture_GPIO_Port GPIOA
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
